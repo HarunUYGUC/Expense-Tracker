10995edf6289a11be76980d2548a481689619b08
Harun Uyguç <harun.uyguc@hotmail.com>
2026-01-23 12:47:27 +0300

    Delete Animation added

--- src/pages/ManualEntry.jsx
+++ src/pages/ManualEntry.jsx
@@ -365,13 +365,15 @@ function ManualEntry() {
                                 ${entry.price.toFixed(2)}
                             </div>
                             
-                            <div style={{ width: '40px', textAlign: 'right' }}>
+                            <div style={{ width: '50px', display: 'flex', justifyContent: 'flex-end' }}>
                               <button 
-                                className="btn btn-link text-danger p-0 border-0"
+                                className="delete-entry-btn"
                                 onClick={(e) => handleDeleteEntry(e, entry.id)}
                                 title="Delete Entry"
                               >
-                                <FaTrash />
+                                <svg viewBox="0 0 448 512" className="delete-entry-svgIcon">
+                                  <path d="M135.2 17.7L128 32H32C14.3 32 0 46.3 0 64S14.3 96 32 96H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H320l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32L53.2 467c1.6 25.3 22.6 45 47.9 45H346.9c25.3 0 46.3-19.7 47.9-45L416 128z"></path>
+                                </svg>
                               </button>
                             </div>
 
